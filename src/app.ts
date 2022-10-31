a2275550d50fd66c0a925cfcc5ac8577674ddab6
dopewevmond <dopewevmond@gmail.com>
2022-10-31 10:06:36 +0000

    added eslint

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,43 @@
-import * as express from 'express';
-import * as bodyparser from 'body-parser';
-import Controller from './controllers/controller.interface';
+import * as express from 'express'
+import * as bodyparser from 'body-parser'
+import Controller from './controllers/controller.interface'
+import * as path from 'path'
 
 class App {
-    public app: express.Application;
+  public app: express.Application
 
-    constructor (controllers: Controller[]) {
-        this.app = express();
-        this.setupApplication();
-        controllers.forEach( controller => {
-            this.app.use('/', controller.router);
-        });
-    }
+  constructor (controllers: Controller[]) {
+    this.app = express()
+    this.setupApplication()
+    controllers.forEach(controller => {
+      this.app.use('/', controller.router)
+    })
+  }
 
-    /**
+  /**
      * Mount middleware and setup view engine
      */
-    private setupApplication(): void {
-        this.app.use(this.loggerMiddleware);
-        this.app.use(bodyparser.urlencoded({extended: true}));
-        this.app.use('/public', express.static(__dirname + '/../public'));
-        this.app.set('views', __dirname + '/../views');
-        this.app.set('view engine', 'pug');
+  private setupApplication (): void {
+    this.app.use(this.loggerMiddleware)
+    this.app.use(bodyparser.urlencoded({ extended: true }))
+    this.app.use('/public', express.static(path.join(__dirname, '/../public')))
+    this.app.set('views', path.join(__dirname, '/../views'))
+    this.app.set('view engine', 'pug')
+  }
 
-    }
-    
-    private loggerMiddleware(request: express.Request, response: express.Response, next) {
-        console.log(`${request.method} ${request.path}`);
-        next();
-    }
+  private loggerMiddleware (request: express.Request, response: express.Response, next): void {
+    const method: string = request.method
+    const path: string = request.path
+    console.log(`${method} ${path}`)
+    next()
+  }
 
-    public listen() {
-        this.app.listen(process.env.PORT || 3000, () => {
-            console.log(`listening on port ${process.env.PORT || 3000}`);
-            
-        })
-    }
+  public listen (): void {
+    const PORT: number | undefined = 3000
+    this.app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`)
+    })
+  }
 }
 
-export default App;
+export default App
