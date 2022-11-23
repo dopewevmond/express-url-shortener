b33d2733df2d268d08d0a85b965c87a3b42db4e4
dopewevmond <dopewevmond@gmail.com>
2022-11-23 15:04:00 +0000

    copy button works for all links now

--- public/script.js
+++ public/script.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const copyButton = document.querySelector(".copy-button");
-
-    if (copyButton) {
-        copyButton.addEventListener('click', (e) => {
-            const short_link = document.querySelector(".short-link").href;
-            navigator.clipboard.writeText(short_link);
-            copyButton.innerHTML = "Copied!";
-        });
-    }
-});
+document.addEventListener('DOMContentLoaded', function (event) {
+  const copyButtons = document.querySelectorAll('.copy-button')
+  const shortLinks = document.querySelectorAll('.short-link')
+  copyButtons.forEach((copyBtn) => {
+    copyBtn.addEventListener('click', (e) => {
+      for (const link of shortLinks) {
+        if (link.dataset.id === copyBtn.dataset.id) {
+          navigator.clipboard.writeText(link.href)
+          copyBtn.innerHTML = 'Copied!'
+        }
+      }
+    })
+  })
+})
