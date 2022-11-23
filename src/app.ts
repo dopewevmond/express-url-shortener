16cbf373e7a2374a90218bc481a1cdb7e45c0def
dopewevmond <dopewevmond@gmail.com>
2022-11-23 18:17:40 +0000

    morgan for logging, error page

--- src/app.ts
+++ src/app.ts
@@ -2,34 +2,37 @@ import * as express from 'express'
 import * as bodyparser from 'body-parser'
 import Controller from './controllers/controller.interface'
 import * as path from 'path'
+import AppError from 'exceptions/exception.apperror'
+import * as morgan from 'morgan'
 
 class App {
   public app: express.Application
+  public shortenedLinks: any[]
 
   constructor (controllers: Controller[]) {
     this.app = express()
+    this.app.use(morgan('dev'))
     this.setupApplication()
     controllers.forEach(controller => {
       this.app.use(controller.path, controller.router)
     })
+    // adding error handling middleware after mounting all controllers
+    this.app.use(this.ErrorHandlerMiddleware)
   }
 
   /**
      * Mount middleware and setup view engine
      */
   private setupApplication (): void {
-    this.app.use(this.loggerMiddleware)
     this.app.use(bodyparser.urlencoded({ extended: true }))
     this.app.use('/public', express.static(path.join(__dirname, '/../public')))
     this.app.set('views', path.join(__dirname, '/../views'))
     this.app.set('view engine', 'pug')
   }
 
-  private loggerMiddleware (request: express.Request, response: express.Response, next: express.NextFunction): void {
-    const method: string = request.method
-    const path: string = request.path
-    console.log(`${method} ${path}`)
-    next()
+  private ErrorHandlerMiddleware (err: AppError, req: express.Request, res: express.Response, next: express.NextFunction): void {
+    // res.status(err.statusCode).json({ message: err.message })
+    res.status(err.statusCode).render('error', { err })
   }
 
   public listen (): void {
